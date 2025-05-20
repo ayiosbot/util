e51e846abd3d2b0d3f0bc3a9ef0beb923bfb8685
DevAX1T <darkweaponwaffles@gmail.com>
2025-05-20 14:48:35 -0700

    Add formatBytes

--- src/StringUtils.ts
+++ src/StringUtils.ts
@@ -9,7 +9,7 @@ export default {
     CURRENCY_SYMBOL: '$',
     CURRENCY_SEPARATOR: '',
     /**
-     * Cuts a string off at {max} displaying ... at the end
+     * Cuts a string off at {max} displaying ... at the end. Adds +3 to the length of string.
      * @param str The string to cutoff
      * @param max The maximum length the string can be before cutting off
      * @returns A cut string length: `max` + 3
@@ -28,6 +28,14 @@ export default {
      */
     formatNumber(integer: number): string {
         return integer.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    },
+    formatBytes(bytes: number, decimalPlaces: number = 2): string {
+        if (bytes === 0) return '0 Bytes';
+
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        const i = Math.floor(Math.log(bytes) / Math.log(1024));
+        const size = (bytes / Math.pow(1024, i)).toFixed(decimalPlaces);
+        return `${size} ${sizes[i]}`;
     },
     /**
      * Capitalize the first letter of a string
