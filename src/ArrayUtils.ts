bfa8043b5cc7e9e687c3ba737316c858493de779
DevAX1T <darkweaponwaffles@gmail.com>
2025-06-07 05:33:16 -0700

    Add array utils

---
+++ src/ArrayUtils.ts
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2025 Ayios. All rights reserved.
+ *  All code within this repository created by Ayios is under MIT license. Other code within
+ *  this repository is under its own respective license which will be displayed within their
+ *  respective files or around the areas of their code.
+ *  See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+export default {
+    /** Remove a single array element */
+    removeIndex<T>(array: T[], index: number): T[] {
+        return array.splice(index, 1) as T[];
+    },
+    /**
+     * Removes an element from an array by its value.
+     * 
+     * This returns the removed element, or undefined if the element was not found.
+     * @param array The array from which to remove the element.
+     * @param element The element to remove from the array.
+     * @returns 
+     */
+    removeElement<T>(array: T[], element: any): T | undefined {
+        const index = array.indexOf(element);
+        if (index > -1) {
+            return this.removeIndex(array, index)[0];
+        }
+
+        return undefined;
+    }
+}
